2d69203689e3e7072919b090cb238a11193d9ac7
Misteremoji <thishoi4idiot@gmail.com>
2026-08-25 00:56:27 -0400

    1 - Initial Commit

---
+++ common/defines/dumm_defines_copy.lua
@@ -0,0 +1,14 @@
+NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 30
+NDefines.NGame.LAG_DAYS_FOR_PAUSE = 18
+NDefines.NGame.GAME_SPEED_SECONDS = { 1.0, 0.25, 0.1, 0.05, 0.0 }
+NDefines.NCountry.EVENT_PROCESS_OFFSET = 30
+NDefines.NMilitary.BASE_DIVISION_BRIGADE_GROUP_COST = 10 	--Base cost to unlock a regiment slot,
+NDefines.NMilitary.BASE_DIVISION_BRIGADE_CHANGE_COST = 3	--Base cost to change a regiment column.
+NDefines.NMilitary.BASE_DIVISION_SUPPORT_SLOT_COST = 5 	--Base cost to unlock a support slot
+
+NDefines_Graphics.NGraphics.MAPICON_GROUP_PASSES = 10
+
+NDefines_Graphics.NGraphics.CAPITAL_ICON_CUTOFF = 800						-- 1100 
+NDefines_Graphics.NGraphics.DRAW_MAP_OBJECTS_CUTOFF = 250 					-- 550 
+NDefines_Graphics.NGraphics.MAP_BUILDINGS_SHRINK_DISTANCE = 100				-- 180
+
---
+++ descriptor.mod
@@ -0,0 +1,7 @@
+version="1.3"
+tags={
+	"Alternative History"
+}
+name="MojisNonHistMod"
+supported_version="1.19.2"
+remote_file_id="3553569019"
