32523918288d7d760cee5465904ad3ade1ff6a84
Misteremoji <thishoi4idiot@gmail.com>
2026-08-29 23:12:06 -0400

    Another General update

--- common/defines/dumm_defines_copy.lua
+++ common/defines/dumm_defines_copy.lua
@@ -2,7 +2,7 @@ NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 30
 NDefines.NGame.LAG_DAYS_FOR_PAUSE = 18
 NDefines.NGame.GAME_SPEED_SECONDS = { 1.0, 0.25, 0.1, 0.05, 0.0 }
 NDefines.NCountry.EVENT_PROCESS_OFFSET = 30
-NDefines.NMilitary.BASE_DIVISION_BRIGADE_GROUP_COST = 10 	--Base cost to unlock a regiment slot,
+NDefines.NMilitary.BASE_DIVISION_BRIGADE_GROUP_COST = 0 	--Base cost to unlock a regiment slot,
 NDefines.NMilitary.BASE_DIVISION_BRIGADE_CHANGE_COST = 3	--Base cost to change a regiment column.
 NDefines.NMilitary.BASE_DIVISION_SUPPORT_SLOT_COST = 5 	--Base cost to unlock a support slot
 
